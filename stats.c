61129616568523d4d0363bc5e8a3c543751438c1
JavEng <ja.engra@gmail.com>
2018-05-16 09:51:32 -0600

    Addind find_minimum sin acabar

--- stats.c
+++ stats.c
@@ -25,34 +25,64 @@
 #include "stats.h"
 
 /* Size of the Data Set */
-#define SIZE (40)
+#define SIZE (4)
+
+unsigned char find_minimum(unsigned char *test);
 
 void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+/*  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
 
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
+*/
+
+
+
+  unsigned char test[4] = { 97, 98, 99, 100};
+
   /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  /* Statistics and Printing Functions Go Here*/
+  unsigned char minimo;
+  char (*pos_tabla);
+  pos_tabla = test;
+  minimo  = find_minimum(test);
+  printf("El mínimo del array es:%c\n",minimo);
+
+
+
 
 }
 
 print_statistics(char *pos_tabla){};
 
-print_array(char *pos_tabla,int tamaño){};
+print_array(char *pos_tabla,int tamano){};
 
-find_median(char *pos_tabla,int tamaño,int media){};
+find_median(char *pos_tabla,int tamano,int media){};
 
-find_mean(char *pos_tabla,int tamaño, int mean){};
+find_mean(char *pos_tabla,int tamano, int mean){};
 
-find_maximum(char *pos_tabla,int tamaño, int max){};
+find_maximum(char *pos_tabla,int tamano, int max){};
 
-find_minimum(char *pos_tabla,int tamaño, int min{});
+unsigned char find_minimum(unsigned char *test)
+{
+	int i;
+	unsigned char min = 255;
+	for (i=0;i<SIZE;i++)
+	{
+		printf("test[%i]:%c\n",i,test[i]);
+		if (min>test[i])
+		{
+			min=test[i];
+			printf("min:%c\n",min);
+		}
+	}
+	return min;
+};
 
-sort_array(char *pos_tabla,int tamaño){}:
+sort_array(char *pos_tabla,int tamano){};
 
 /* Add other Implementation File Code Here */
